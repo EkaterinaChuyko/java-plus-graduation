04a0df75e1c6181263fdf9ae03cf3dcb68ecd2bb
EkaterinaChuyko <kylefoxmay@yandex.ru>
2026-05-02 20:15:29 +0300

    Исправлена проверка при создании категории

--- core/main-service/src/main/java/ru/practicum/main/service/category/CategoryServiceImpl.java
+++ core/main-service/src/main/java/ru/practicum/main/service/category/CategoryServiceImpl.java
@@ -27,6 +27,10 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     @Transactional
     public CategoryDto createCategory(NewCategoryDto newCategoryDto) {
+
+        if (categoryRepository.existsByName(newCategoryDto.getName())) {
+            throw new ConflictException("Category already exists");
+        }
         Category category = CategoryMapper.toEntity(newCategoryDto);
         return CategoryMapper.toDto(categoryRepository.save(category));
     }
