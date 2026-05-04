2178ae03d7e71b441445a187d8eb42382479eedc
EkaterinaChuyko <kylefoxmay@yandex.ru>
2026-05-04 15:50:56 +0300

    Исправлен контроллер, добавлена валидация

--- stats/stats-server/src/main/java/ru/practicum/stats/server/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/server/controller/StatsController.java
@@ -2,7 +2,9 @@ package ru.practicum.stats.server.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import ru.practicum.stats.dto.HitDto;
@@ -10,11 +12,11 @@ import ru.practicum.stats.dto.ViewStatsDto;
 import ru.practicum.stats.server.service.StatsService;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 public class StatsController {
 
     private final StatsService statsService;
@@ -28,31 +30,25 @@ public class StatsController {
 
     @GetMapping("/stats")
     public List<ViewStatsDto> getStats(
-            @RequestParam String start,
-            @RequestParam String end,
-            @RequestParam(required = false) List<String> uris,
-            @RequestParam(defaultValue = "false") boolean unique
+            @RequestParam
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime start,
+
+            @RequestParam
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime end,
+
+            @RequestParam(required = false)
+            List<String> uris,
+
+            @RequestParam(defaultValue = "false")
+            boolean unique
     ) {
-        if (start == null || end == null) {
+
+        if (end.isBefore(start)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        LocalDateTime startDate;
-        LocalDateTime endDate;
-
-        try {
-            startDate = LocalDateTime.parse(start, formatter);
-            endDate = LocalDateTime.parse(end, formatter);
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-
-        if (endDate.isBefore(startDate)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-
-        return statsService.getStats(startDate, endDate, uris, unique);
+        return statsService.getStats(start, end, uris, unique);
     }
 }
