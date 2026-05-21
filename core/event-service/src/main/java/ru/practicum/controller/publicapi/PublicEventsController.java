53a678a50ab1aae2978c63e9189aabad15f99905
EkaterinaChuyko <kylefoxmay@yandex.ru>
2026-05-21 19:03:48 +0300

    исправлен контроллер

--- core/event-service/src/main/java/ru/practicum/controller/publicapi/PublicEventsController.java
+++ core/event-service/src/main/java/ru/practicum/controller/publicapi/PublicEventsController.java
@@ -27,8 +27,12 @@ public class PublicEventsController {
             @RequestParam(required = false) String text,
             @RequestParam(required = false) List<Long> categories,
             @RequestParam(required = false) Boolean paid,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeStart,
-            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeEnd,
+            @RequestParam(required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime rangeStart,
+            @RequestParam(required = false)
+            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+            LocalDateTime rangeEnd,
             @RequestParam(defaultValue = "false") Boolean onlyAvailable,
             @RequestParam(defaultValue = "EVENT_DATE") String sort,
             @RequestParam(defaultValue = "0") @Min(0) Integer from,
@@ -36,11 +40,24 @@ public class PublicEventsController {
             HttpServletRequest req
     ) {
 
-        if (rangeStart != null && rangeEnd != null &&
-            rangeStart.isAfter(rangeEnd)) {
-            throw new ConditionsException("rangeStart must be before rangeEnd");
+        if (rangeStart != null && rangeEnd != null && rangeStart.isAfter(rangeEnd)) {
+            rangeStart = null;
+            rangeEnd = null;
         }
-        return service.searchPublic(text, categories, paid, rangeStart, rangeEnd, onlyAvailable, sort, from, size, req.getRequestURI(), req.getRemoteAddr());
+
+        return service.searchPublic(
+                text,
+                categories,
+                paid,
+                rangeStart,
+                rangeEnd,
+                onlyAvailable,
+                sort,
+                from,
+                size,
+                req.getRequestURI(),
+                req.getRemoteAddr()
+        );
     }
 
     @GetMapping("/{id}")
